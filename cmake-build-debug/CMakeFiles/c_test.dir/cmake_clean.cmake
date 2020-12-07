7c9fda68f2764a5b365c79198c238c5fb44990a9
uestc_yhr <961154789@qq.com>
2020-12-07 16:26:57 +0800

    配置文件

---
+++ cmake-build-debug/CMakeFiles/c_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_test.dir/main.c.obj"
+  "c_test.exe"
+  "c_test.exe.manifest"
+  "c_test.pdb"
+  "libc_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
