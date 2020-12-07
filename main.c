70ec95f4790f927d2a89b1db4d4845493b9a9c42
uestc_yhr <961154789@qq.com>
2020-12-07 16:30:20 +0800

    主程序

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "algorithm/recursive-algorithm/divide-and-conquer-algorithm/mergequeue.h"
+#include "algorithm/recursive-algorithm/divide-and-conquer-algorithm/speedqueue.h"
+#include "algorithm/recursive-algorithm/divide-and-conquer-algorithm/rankselect.h"
+#include "algorithm/recursive-algorithm/divide-and-conquer-algorithm/routedate.h"
+#include "algorithm/recursive-algorithm/dynamic-planning/mulit-matrix.h"
+#include "algorithm/recursive-algorithm/dynamic-planning/longest-common-subsequence.h"
+#include "algorithm/recursive-algorithm/dynamic-planning/zero-one-knapsack-problem.h"
+#include "algorithm/recursive-algorithm/dynamic-planning/optima-binary-search-tree.h"
+#include "algorithm/greedy-algorithm/event-scheduling-issues.h"
+
+int main() {
+    /*递归算法*/
+    // 1.分治法
+    // merge_queue(); // 合并排序
+    // speedqueue(); // 快速排序
+    // rankselect(); //线性时间选择问题
+    // routedate(); //循环赛日程表问题
+
+    // 2.动态规划算法
+    // multimatrix(); // 矩阵连乘问题
+    // LCS(); // 最长公共子序列问题
+    // zero_one_bag(); // 0/1背包问题
+    // OBST(); // 最优二叉查找树
+// --------------------------------------------------------------------------------
+    /*贪心算法*/
+    // ESI(); // 活动安排问题
+    return 0;
+}
+
